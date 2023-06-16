844b79516c492875093641614bb19460909a4f43
SGetmanova <getmanovas@mail.ru>
2023-06-16 11:18:55 +0300

    init simple project

---
+++ src/main/java/com/example/gitsimpleproject1/Gitsimpleproject1Application.java
@@ -0,0 +1,13 @@
+package com.example.gitsimpleproject1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gitsimpleproject1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Gitsimpleproject1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gitsimpleproject1/Gitsimpleproject1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gitsimpleproject1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gitsimpleproject1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
